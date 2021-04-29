484a25cdd129dbff747cc0d1aa8a50aec8e591e6
Vacant-name <nikitamolchanov315@mail.ru>
2021-04-29 20:31:28 +0300

    methods of 1st lesson

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,31 +1,38 @@
+import javax.sound.midi.Soundbank;
+import java.sql.SQLOutput;
+import java.util.Arrays;
+
+
 /**
  * Array based storage for Resumes
  */
 
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
-    int emptyPlace;
-    int saveCounterResume;
-    int sizeStorage;
-    int getAllCounter;
+    int resumeNumbers;
+
     void clear() {
-        while(emptyPlace <= sizeStorage) { // обнуляем каждую занятую ячейку storage
-            storage[emptyPlace] = null;
-            emptyPlace++;
+        while(resumeNumbers != 0) { // обнуляем каждую занятую ячейку storage
+            storage[resumeNumbers] = null;
+            resumeNumbers--;
         }
     }
 
-    void save(Resume r) {
-        if(storage[saveCounterResume] == null) { // если в ячейке под индексом saveCounterResume пусто
-            storage[saveCounterResume] = r; // вставляем в пустую ячейку резюме
-            saveCounterResume++;
+    void save(Resume r) { // тип рюземе содержит строку
+        if(storage[resumeNumbers] == null) { // если в ячейке под индексом resumeNumbers пусто
+            storage[resumeNumbers] = r; // вставляем в пустую ячейку резюме
+            resumeNumbers++;
         } else {
             System.out.println("Resume storage is full");
         }
     }
 
-     Resume get(String uuid) {
-         return storage[saveCounterResume];
+    Resume get(String uuid) {
+        int i = 0;
+        if(storage[i] != null) {
+
+        }
+        return storage[i];
     }
 
     void delete(String uuid) {
@@ -36,16 +43,13 @@ public class ArrayStorage {
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        for (int i = 0; i <= sizeStorage; i++) {
-            getAllCounter = i;
+        for(int i = 0; i < resumeNumbers; i++) {
+            System.out.println(storage[i]);
         }
-        return new Resume[getAllCounter];
+        return null;
     }
 
     int size() {
-        while (sizeStorage != saveCounterResume) {
-            sizeStorage++;
-        }
-        return sizeStorage;
+        return resumeNumbers;
     }
 }
