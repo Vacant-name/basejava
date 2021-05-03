9ae778d77829dc97f76f54fbe03ad240a6eef5a4
Vacant-name <nikitamolchanov315@mail.ru>
2021-05-03 12:04:46 +0300

    methods of 1st lesson v.3

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,7 +1,7 @@
-import javax.sound.midi.Soundbank;
-import java.sql.SQLOutput;
-import java.util.Arrays;
 
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Array based storage for Resumes
@@ -12,14 +12,14 @@ public class ArrayStorage {
     int resumeNumbers;
 
     void clear() {
-        while(resumeNumbers != 0) { // обнуляем каждую занятую ячейку storage
+        while (resumeNumbers != 0) { // обнуляем каждую занятую ячейку storage
             storage[resumeNumbers] = null;
             resumeNumbers--;
         }
     }
 
     void save(Resume r) { // тип рюземе содержит строку
-        if(storage[resumeNumbers] == null) { // если в ячейке под индексом resumeNumbers пусто
+        if (storage[resumeNumbers] == null) { // если в ячейке под индексом resumeNumbers пусто
             storage[resumeNumbers] = r; // вставляем в пустую ячейку резюме
             resumeNumbers++;
         } else {
@@ -29,27 +29,37 @@ public class ArrayStorage {
 
     Resume get(String uuid) {
         int i = 0;
-        if(storage[i] != null) {
 
+        while (storage[i].uuid != uuid) { // содержание информации в i-ом элементе массива
+            i++;
         }
         return storage[i];
     }
 
-//    void delete(String uuid) {
-//        uuid = null;
-//    }
-//
-//    /**
-//     * @return array, contains only Resumes in storage (without null)
-//     */
-//    Resume[] getAll() {
-//        for(int i = 0; i < resumeNumbers; i++) {
-//            System.out.println(storage[i]);
-//        }
-//        return null;
-//    }
-//
-//    int size() {
-//        return resumeNumbers;
-//    }
+    void delete(String uuid) {
+        int i = 0;
+
+        while (storage[i].uuid != uuid) { // содержание информации в i-ом элементе массива
+            i++;
+        }
+        storage[i] = null;
+
+        while (storage[i + 1] != null) { // сдвигаем все резюме на одну позицию влево на место удаленного
+            storage[i] = storage[i + 1];
+            i++;
+        }
+    }
+
+    /**
+     * @return array, contains only Resumes in storage (without null)
+     */
+    Resume[] getAll() {
+        Resume[] allResumes = new Resume[resumeNumbers];
+        allResumes = Arrays.copyOf(storage, resumeNumbers);
+        return allResumes;
+    }
+
+    int size() {
+        return resumeNumbers;
+    }
 }
